4a8e5f2a8a64ce08a98a7d2326764894374dde17
Joseph Perry <josephperry720@gmail.com>
2020-10-08 19:53:17 -0400

    ajax function for put request

--- public/assets/js/sushi.js
+++ public/assets/js/sushi.js
@@ -1,5 +1,19 @@
 $(function() {
-    
+    $(".eat-button").on("click", function() {
+        const id = $(this).data("id");
+        const state = $(this).data("bool");
+
+        const stateObj = {
+            eaten: !state,
+        }
+
+        $.ajax("api/sushi/" + id, {
+            type: "PUT",
+            data: stateObj
+        }).then(function() {
+            location.reload();
+        })
+    })
 
 
 
