b1fb83ebe159c767084ecf0f80edbbc52ee7591c
Joseph Perry <josephperry720@gmail.com>
2020-10-08 16:24:33 -0400

    added sql table schema

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS sushi_db;
+
+CREATE DATABASE sushi_db;
+
+use sushi_db;
+
+CREATE TABLE sushis (
+	id INT AUTO_INCREMENT NOT NULL,
+    sushi_name VARCHAR(30) NOT NULL,
+    eaten BOOLEAN NOT NULL,
+    PRIMARY KEY (id)
+);
+
